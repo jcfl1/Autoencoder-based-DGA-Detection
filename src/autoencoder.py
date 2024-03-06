192821f65095754412cc92cf24f0b4b1280bedce
Júlio César <jcfl@cin.ufpe.br>
2024-03-06 20:01:32 -0300

    Obtaining AE detection results, fixing bugs in autoencoder code and updating environment.yml

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -16,18 +16,19 @@ class EarlyStopping:
     self.counter = 0
     self.early_stop = False
     self.objective = objective
-    self.best_score = np.Inf
     self.path = path
 
     if self.objective == 'minimize':
-        self.compare_funct = lambda new_score,best_score: new_score < best_score
+        self.compare_funct = lambda new_score,best_score: new_score < best_score - self.delta
+        self.best_score = np.Inf
     elif self.objective == 'maximize':
-        self.compare_funct = lambda new_score,best_score: new_score > best_score
+        self.compare_funct = lambda new_score,best_score: new_score > best_score + self.delta
+        self.best_score = -np.Inf
     else:
         raise ValueError(f"Unexpected value atributted to 'objective'.")
 
   def __call__(self, new_val_score, model):
-    if self.compare_funct(new_val_score, self.best_score - self.delta):     # If comparison between new_val_score and best_score is aligned with our objetive, then lets save current checkpoint and update best_score
+    if self.compare_funct(new_val_score, self.best_score):     # If comparison between new_val_score and best_score is aligned with our objetive, then lets save current checkpoint and update best_score
         self.save_checkpoint(new_val_score, model)
         self.counter = 0
     else:                                                                   # If comparison between new_val_score and best_score is NOT aligned with our objetive, lets increment patient counter
@@ -38,7 +39,7 @@ class EarlyStopping:
 
   def save_checkpoint(self, new_val_score, model):
     if self.verbose:
-        print(f'Validation score decreased ({self.best_score:.5f} --> {new_val_score:.5f}).  Saving model ...')
+        print(f'Validation score improved ({self.best_score:.5f} --> {new_val_score:.5f}).  Saving model ...')
     torch.save(model, self.path)
     self.best_score = new_val_score
 
@@ -50,11 +51,11 @@ class Autoencoder(nn.Module):
 
     self.in_features = in_features
     self.dropout_rate = dropout_rate
-    self.early_stopping = None
+    self.activation = nn.ReLU()
 
     # Encoder
     self.encoder = nn.ModuleList()
-    self.encoder.append(nn.Linear(self.input_dim, hidden_layers_dims[0]))
+    self.encoder.append(nn.Linear(self.in_features, hidden_layers_dims[0]))
     self.encoder.append(self.activation)
     for i in range(0, len(hidden_layers_dims)//2 ):
         self.encoder.append(nn.Linear(hidden_layers_dims[i], hidden_layers_dims[i+1]))
@@ -63,18 +64,23 @@ class Autoencoder(nn.Module):
     # Decoder
     self.decoder = nn.ModuleList()
     for i in range(len(hidden_layers_dims)//2, len(hidden_layers_dims)-1):
-        self.decoer.append(nn.Linear(hidden_layers_dims[i], hidden_layers_dims[i+1]))
-        self.decoer.append(self.activation)
-    self.decoer.append(nn.Linear(hidden_layers_dims[-1], self.output_dim))        
-    self.decoer.append(nn.Sigmoid())
+        self.decoder.append(nn.Linear(hidden_layers_dims[i], hidden_layers_dims[i+1]))
+        self.decoder.append(self.activation)
+    self.decoder.append(nn.Linear(hidden_layers_dims[-1], self.in_features))        
 
   def forward(self, X):
-    encoded = self.encoder(X)
-    decoded = self.decoder(encoded)
+    encoded = X
+    for layer in self.encoder:
+      encoded = layer(encoded)
+    
+    decoded = encoded
+    for layer in self.decoder:
+      decoded = layer(decoded)
+    
     return decoded
 
   def compile(self, learning_rate, weight_decay):
-    self.criterion = nn.MSEscore()
+    self.criterion = nn.MSELoss()
     self.optimizer = optim.Adam(self.parameters(), lr = learning_rate, weight_decay=weight_decay)
 
   def fit(self, X_train, num_epochs, batch_size, X_val = None, y_val=None, patience = None, delta = None, es_criterion=None):
@@ -83,10 +89,10 @@ class Autoencoder(nn.Module):
       if es_criterion == 'loss':
          es_objective = 'minimize'
       elif es_criterion == 'aucroc':
-         es_objective == 'maximize'
+         es_objective = 'maximize'
       else:
         raise ValueError(f"Unexpected value atributted to 'es_criterion'.")
-      self.early_stopping = EarlyStopping(patience, delta, objective=es_objective)
+      early_stopping = EarlyStopping(patience, delta, objective=es_objective)
 
     val_avg_losses = []
     train_avg_losses = []
@@ -109,31 +115,31 @@ class Autoencoder(nn.Module):
       print(f'Epoch#{epoch+1}: Train Average Loss = {train_avg_loss:.5f}')
 
       # Early stopping mechanism
-      if self.early_stopping is not None:
+      if early_stopping is not None:
         val_losses = []
         self.eval()
         with torch.no_grad():
           for batch in range(0, len(X_val), batch_size):
             batch_X = X_val[batch:(batch+batch_size)]
             batch_reconstruction = self.forward(batch_X)
-            new_val_anomaly_score = self.criterion(batch_reconstruction, batch_X)
-            val_losses.append(new_val_anomaly_score.item())
+            batch_val_anomaly_score = torch.mean(torch.pow(batch_reconstruction - batch_X, 2), dim=1).tolist()
+            val_losses.extend(batch_val_anomaly_score)
         val_avg_loss = np.mean(val_losses)
         val_avg_losses.append(val_avg_loss)
-        
+
         if es_criterion == 'aucroc':
             val_aucroc = roc_auc_score(y_val, val_losses)
             es_score = val_aucroc
         elif es_criterion == 'loss':
             es_score = val_avg_loss
         
-        self.early_stopping(es_score, self)
+        early_stopping(es_score, self)
 
-        if self.early_stopping.early_stop:
+        if early_stopping.early_stop:
           print(f'Stopped by early stopping at epoch {epoch+1}')
           break
 
-    if self.early_stopping is not None:
-      self = torch.load('checkpoint.pt')
-    self.eval()
-    return train_avg_losses, val_avg_losses
+    if early_stopping is not None:
+      checkpoint_model = torch.load('checkpoint.pt')
+    checkpoint_model.eval()
+    return checkpoint_model, train_avg_losses, val_avg_losses
