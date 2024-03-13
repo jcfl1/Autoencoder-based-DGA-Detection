3fe3bae6bedf689200037f60a94e9c5076fa72a7
Júlio César <jcfl@cin.ufpe.br>
2024-03-12 23:49:01 -0300

    Adding new datasets experiments

--- data_setup.sh
+++ data_setup.sh
@@ -5,3 +5,6 @@ wget 'http://s3-us-west-1.amazonaws.com/umbrella-static/top-1m.csv.zip'
 
 # OSINT (Bambenek consulting feeds) dataset download
 wget 'https://faf.bambenekconsulting.com/feeds/dga-feed.txt' -O bambenek_dga_feed.txt
+
+# Majestic Million
+wget 'https://downloads.majestic.com/majestic_million.csv'
